c76c11b858cd70caa089d313d636eec3fd9047b0
DawnBreaker <tunganhngo207@gmail.com>
2024-04-14 16:26:53 +0700

    #8 -update route navigate

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,32 @@
 // import { useState } from 'react'
-import './App.css';
-import Header from './components/Header/Header.tsx';
-import Home from './pages/Home.tsx';
-import Footer from './components/Footer/Footer.tsx';
+import "./App.css";
+import Header from "./components/Header/Header.tsx";
+import Footer from "./components/Footer/Footer.tsx";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home.tsx";
+import Register from "./pages/Register.tsx";
+import Login from "./pages/Login.tsx";
+import Products from "./pages/Products.tsx";
+import NotFound from "./pages/NotFound.tsx";
+import Dashboard from "./pages/admin/Dashboard.tsx";
 
 const App = () => {
   return (
     <>
       <Header />
-      <Home />
+      <Routes>
+        <Route path="/">
+          <Route index element={<Home />} />
+          <Route path="/product" element={<Products />} />
+          <Route path="/signup" element={<Register />} />
+          <Route path="/signin" element={<Login />} />
+          <Route />
+        </Route>
+        <Route path="/admin">
+          <Route index element={<Dashboard />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
       <Footer />
     </>
   );
